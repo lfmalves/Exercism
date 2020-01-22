8d0c492856cd0ce71755d2c16c052569b1e0e1aa
LFM Alves <luis.projensino@gmail.com>
2020-01-22 17:23:42 -0300

    leap erlang

--- erlang/leap/src/leap.erl
+++ erlang/leap/src/leap.erl
@@ -3,4 +3,20 @@
 -export([leap_year/1]).
 
 
-leap_year(_Year) -> undefined.
+leap_year(Year) -> 
+  if
+  	(Year rem 4) == 0 ->
+  		if
+  			(Year rem 100) /= 0 ->
+  			true;
+  		true ->
+  		  if
+  		  	(Year rem 400) == 0 ->
+  		  		true;
+  		  true ->
+  		    false
+  		  end,
+  		end,
+  true -> 
+    false
+  end.
