088779070f20f344dc7db38458d24416ccfd82eb
LFM Alves <luis.projensino@gmail.com>
2020-01-23 12:51:11 -0300

    refactoring

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,30 +1,22 @@
-class ResistorColorDuo 
-	def self.value(resistors)
-		total = []
-		resistors.each do |color|
-			case color
-				when "black"
-					total << "0"
-				when "brown"
-					total << "1"
-				when "red"
-					total << "2"
-				when "orange"
-					total << "3"
-				when "yellow"
-					total << "4"
-				when "green"
-					total << "5"
-				when "blue"
-					total << "6"
-				when "violet"
-					total << "7"
-				when "grey"
-					total << "8"
-				when "white"
-					total << "9"
-			end
-		end
-		total[0..1].join("").to_i
-	end
+# frozen_string_literal: true
+
+class ResistorColorDuo
+  def self.value(resistors)
+    total = []
+    resistors.each do |color|
+      total << case color
+      when 'black' then 0
+      when 'brown' then 1
+      when 'red' then 2
+      when 'orange' then 3
+      when 'yellow' then 4
+      when 'green' then 5
+      when 'blue' then 6
+      when 'violet' then 7
+      when 'grey' then 8
+      when 'white' then 9
+      end
+    end
+    total[0..1].join('').to_i
+  end
 end
