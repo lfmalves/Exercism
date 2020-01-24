b2fdf134d9bf32addc630bbdb94027aee2402e9c
LFM Alves <luis.projensino@gmail.com>
2020-01-24 18:38:35 -0300

    improvement for resistor_color_duo

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -2,24 +2,22 @@
 
 class ResistorColorDuo
   @map_color_to_value = {
-      'black' => 0,
-      'brown' => 1,
-      'red' => 2,
-      'orange' => 3,
-      'yellow' => 4,
-      'green' => 5,
-      'blue' => 6,
-      'violet' => 7,
-      'grey' => 8,
-      'white' => 9
+    'black' => 0,
+    'brown' => 1,
+    'red' => 2,
+    'orange' => 3,
+    'yellow' => 4,
+    'green' => 5,
+    'blue' => 6,
+    'violet' => 7,
+    'grey' => 8,
+    'white' => 9
   }
 
   def self.value(resistors)
     total = []
     resistors[0..1].each do |color|
-      @map_color_to_value.each do |x, y|
-        total << y if x == color
-      end
+      total << @map_color_to_value[color]
     end
     total.join.to_i
   end
