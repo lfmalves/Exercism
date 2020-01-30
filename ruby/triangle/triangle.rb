a39fe7b29d93017e987f9470a9a22697d1583b09
LFM Alves <luis.projensino@gmail.com>
2020-01-30 19:40:26 -0300

    triangle solutions

--- ruby/triangle/triangle.rb
+++ ruby/triangle/triangle.rb
@@ -1,2 +1,35 @@
 class Triangle
+	attr_reader :angles
+
+	def initialize(angles)
+		@angles = angles
+	end
+
+	def equilateral?
+		if valid?
+      angles.uniq.count == 1
+    end
+	end
+
+	def isosceles?
+		if valid?
+		  angles.uniq.count <= 2
+		end
+	end
+
+	def scalene?
+		if valid?
+		  angles.uniq.count == 3	
+		end	
+	end	
+
+	def valid?
+		if angles.include?(0)
+			false
+		elsif angles.max > (angles.min + angles.sort[1])
+			false
+		else
+			true
+		end
+	end
 end
