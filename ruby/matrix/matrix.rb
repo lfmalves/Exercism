98ac1d99065b85b51110f3d9eb53a54aeb213416
LFM Alves <luis.projensino@gmail.com>
2020-01-30 22:06:56 -0300

    matrix solution

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -1,7 +1,18 @@
-class Matrix
-	def rows
-	end
+# frozen_string_literal: true
 
-	def columns
-	end
+class Matrix
+  attr_reader :matrix
+
+  def initialize(matrix)
+    @matrix = matrix
+  end
+
+  def rows
+    num = matrix.split("\n")[0].split.count
+    matrix.split.map(&:to_i).each_slice(num).to_a
+  end
+
+  def columns
+    rows.transpose
+  end
 end
